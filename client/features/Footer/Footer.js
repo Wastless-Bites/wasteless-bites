ff1c602a655d12ad1a8187e56ae11a61f6b235e2
Steven Lee <sehyun493@gmail.com>
2023-05-05 15:52:17 -0400

    Footer React

---
+++ client/features/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-links">
+        <a href="#">Contact | </a>
+        <a href="#">About Us | </a>
+        <a href="#">Social Media </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

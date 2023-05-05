5de8d1c5d0e8ffbdc119cc5b59facff19321fd0c
NoelDiaz1 <Noeldr183@gmail.com>
2023-05-05 16:46:31 -0400

    fixed error with class instead of className

--- client/features/Footer/Footer.js
+++ client/features/Footer/Footer.js
@@ -7,16 +7,16 @@ const Footer = () => {
                 <a href="/contact">Contact | </a>
                 <a href="/aboutus">About Us | </a>
                 <a href="#">
-                    <i class="fa-brands fa-instagram"></i>
+                    <i className="fa-brands fa-instagram"></i>
                 </a>
                 <a href="#">
-                    <i class="fa-brands fa-facebook"></i>
+                    <i className="fa-brands fa-facebook"></i>
                 </a>
                 <a href="#">
-                    <i class="fa-brands fa-twitter"></i>
+                    <i className="fa-brands fa-twitter"></i>
                 </a>
                 <a href="#">
-                    <i class="fa-brands fa-linkedin"></i>
+                    <i className="fa-brands fa-linkedin"></i>
                 </a>
             </div>
         </footer>

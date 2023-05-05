e539aeb2ba86ec1c117e63515bd5dbcad3ae357f
Richard <Richman962@gmail.com>
2023-05-05 15:53:40 -0400

    Added Navbar component

---
+++ client/features/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;

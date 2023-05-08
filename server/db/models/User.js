5772b439e2d159d26b1310883f0fc3b0d3abb7b7
Richard <Richman962@gmail.com>
2023-05-08 11:26:51 -0400

    Made associations and seed file

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,44 +1,48 @@
-const Sequelize = require('sequelize')
-const db = require('../db')
+const Sequelize = require("sequelize");
+const db = require("../db");
 
-const User = db.define('User', {
-    name: {
-        type: Sequelize.TEXT,
-        allowNull: false,
-        validate: {
-            notNull: true,
-            notEmpty: true,
-        },
+const User = db.define("User", {
+  name: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notNull: true,
+      notEmpty: true,
     },
-    email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
-            notNull: true,
-            isEmail: true,
-            notEmpty: true,
-            unique: true,
-        },
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      notNull: true,
+      isEmail: true,
+      notEmpty: true,
     },
-    address: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true,
-            notNull: true,
-        },
+  },
+  address: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      notNull: true,
     },
-    description: {
-        type: Sequelize.TEXT,
-        validate: {
-            max: 100,
-        },
+  },
+  description: {
+    type: Sequelize.TEXT,
+    validate: {
+      max: 100,
     },
-    imageUrl: {
-        type: Sequelize.STRING,
-        defaultValue:
-            'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
-    },
-})
+  },
+  userType: {
+    type: Sequelize.ENUM("admin", "organization", "individual"),
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue:
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+  },
+});
 
-module.exports = User
+module.exports = User;

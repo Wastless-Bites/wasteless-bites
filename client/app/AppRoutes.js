2a9c35b9c033b9ca4da45551a03b9a63c68e9072
NoelDiaz1 <Noeldr183@gmail.com>
2023-05-09 15:27:28 -0400

    more styling. Added singlepost component with map api

--- client/app/AppRoutes.js
+++ client/app/AppRoutes.js
@@ -1,21 +1,23 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Landing from "../features/Landing/Landing";
-import Contact from "../features/Contact/Contact.js";
-import AboutUs from "../features/AboutUs/AboutUs.js";
-import Login from "../features/Login/Login.js";
-import SignUp from "../features/SignUp/SignUp.js";
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Landing from '../features/Landing/Landing'
+import Contact from '../features/Contact/Contact.js'
+import AboutUs from '../features/AboutUs/AboutUs.js'
+import Login from '../features/Login/Login.js'
+import SignUp from '../features/SignUp/SignUp.js'
+import SinglePost from '../features/SinglePost/SinglePost'
 
 const AppRoutes = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Landing />}></Route>
-      <Route path="/contact" element={<Contact />}></Route>
-      <Route path="/aboutus" element={<AboutUs />}></Route>
-      <Route path="/login" element={<Login />}></Route>
-      <Route path="/signup" element={<SignUp />}></Route>
-    </Routes>
-  );
-};
+    return (
+        <Routes>
+            <Route path="/" element={<Landing />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+            <Route path="/aboutus" element={<AboutUs />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/signup" element={<SignUp />}></Route>
+            <Route path="/singlePost" element={<SinglePost />}></Route>
+        </Routes>
+    )
+}
 
-export default AppRoutes;
+export default AppRoutes

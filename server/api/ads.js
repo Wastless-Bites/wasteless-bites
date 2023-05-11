872771df18896fb76d7d875473bc6e104ffd8f32
Richard <Richman962@gmail.com>
2023-05-11 16:53:38 -0400

    Authentication for ad posting/deleting.

--- server/api/ads.js
+++ server/api/ads.js
@@ -1,48 +1,86 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 const {
-    models: { Ad, User },
-} = require('../db')
-const { requireToken, ensureOrganization } = require('../auth/middleware')
+  models: { Ad, User },
+} = require("../db");
 
-router.get('/', async (req, res, next) => {
-    try {
-        const ads = await Ad.findAll({
-            include: [
-                {
-                    model: User,
-                    as: 'organization',
-                },
-            ],
-        })
-        res.json(ads)
-    } catch (err) {
-        next(err)
+router.get("/", async (req, res, next) => {
+  try {
+    const ads = await Ad.findAll({
+      include: [
+        {
+          model: User,
+          as: "organization",
+        },
+      ],
+    });
+    res.json(ads);
+  } catch (err) {
+    next(err);
+  }
+});
+
+const requireOrganization = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send("No authorization token provided");
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const user = await User.findByToken(token);
+    if (user.userType !== "organization") {
+      return res.status(403).send("User is not an organization");
     }
-})
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).send("Invalid token");
+  }
+};
 
-router.post('/', ensureOrganization, async (req, res, next) => {
-    try {
-        console.log(req.body)
-        const newAd = await Ad.create(req.body)
-        res.json(newAd)
-    } catch (error) {
-        next(error)
+router.post("/", requireOrganization, async (req, res, next) => {
+  try {
+    console.log(req.body);
+    const newAd = await Ad.create(req.body);
+    res.json(newAd);
+  } catch (error) {
+    next(error);
+  }
+});
+
+const requireAdOwner = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send("No authorization token provided");
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const user = await User.findByToken(token);
+    const ad = await Ad.findOne({ where: { id: req.params.id } });
+    if (!ad) {
+      return res.status(404).send("Ad not found");
     }
-})
-
-router.delete('/:id', async (req, res, next) => {
-    try {
-        const ad = await Ad.findByPk(req.params.id)
-        if (ad) {
-            await ad.destroy()
-            res.status(204).send()
-        } else {
-            res.status(404).send('Ad not found')
-        }
-    } catch (error) {
-        next(error)
+    if (ad.organizationId !== user.id) {
+      return res.status(403).send("User is not the owner of the ad");
     }
-})
+    next();
+  } catch (error) {
+    return res.status(401).send("Invalid token");
+  }
+};
 
-module.exports = router
+router.delete("/:id", requireAdOwner, async (req, res, next) => {
+  try {
+    const ad = await Ad.findByPk(req.params.id);
+    if (ad) {
+      await ad.destroy();
+      res.status(204).send();
+    } else {
+      res.status(404).send("Ad not found");
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;

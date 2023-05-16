7572fa94beea2547da5d27d36ff6cf1e81a16d41
NoelDiaz1 <Noeldr183@gmail.com>
2023-05-16 13:31:46 -0400

    styled about us wallpaper and sign up page

--- client/features/AboutUs/AboutUs.js
+++ client/features/AboutUs/AboutUs.js
@@ -24,40 +24,41 @@ const AboutUs = () => {
     return (
         <>
             <Navbar />
+            <div className="about-us-container">
+                <div className="about-us-container-section-one">
+                    {aboutUsData.map((data) => (
+                        <div className="about-us-image-container">
+                            <a href={data.linkedin} target="_blank">
+                                <img
+                                    className="about-us-image"
+                                    src={data.imgUrl}
+                                    alt={data.name}
+                                />
+                            </a>
+                            <p className="about-us-name">{data.name}</p>
+                        </div>
+                    ))}
+                </div>
 
-            <div className="about-us-container-section-one">
-                {aboutUsData.map((data) => (
-                    <div className="about-us-image-container">
-                        <a href={data.linkedin} target="_blank">
-                            <img
-                                className="about-us-image"
-                                src={data.imgUrl}
-                                alt={data.name}
-                            />
-                        </a>
-                        <p className="about-us-name">{data.name}</p>
-                    </div>
-                ))}
+                <div className="about-us-container-section-two">
+                    <h1>About Us</h1>
+                    <p className="about-us-text">
+                        As a non-profit organization committed to promoting
+                        sustainability and reducing food waste, we know
+                        firsthand how important it is to get restaurants
+                        involved in this effort. By working together with
+                        restaurants, we can create a more eco-friendly and
+                        socially responsible food system that benefits everyone.
+                        Our approach involves encouraging restaurants to
+                        introduce special offers, like reduced-price meals or
+                        donating surplus food, which helps to reduce waste and
+                        promote a positive image for the restaurant as a
+                        socially responsible business. With your support, we can
+                        continue to raise awareness about food waste and create
+                        a more sustainable future for us all.
+                    </p>
+                </div>
             </div>
-
-            <div className="about-us-container-section-two">
-                <h1>About Us</h1>
-                <p className="about-us-text">
-                    As a non-profit organization committed to promoting
-                    sustainability and reducing food waste, we know firsthand
-                    how important it is to get restaurants involved in this
-                    effort. By working together with restaurants, we can create
-                    a more eco-friendly and socially responsible food system
-                    that benefits everyone. Our approach involves encouraging
-                    restaurants to introduce special offers, like reduced-price
-                    meals or donating surplus food, which helps to reduce waste
-                    and promote a positive image for the restaurant as a
-                    socially responsible business. With your support, we can
-                    continue to raise awareness about food waste and create a
-                    more sustainable future for us all.
-                </p>
-            </div>
-
             <Footer />
         </>
     )
